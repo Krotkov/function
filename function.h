7b86481f8cf89b52e977cd93c6adb39ec8181f10
Krotkov <32042917+Krotkov@users.noreply.github.com>
2019-01-26 13:43:37 +0300

    Update function.h

--- function.h
+++ function.h
@@ -7,6 +7,15 @@
 
 #include <iostream>
 #include <memory>
+#include <cstring>
+
+const int FUNCTION_BUFFER_SIZE = 64;
+
+enum TYPE {
+    EMPTY = 0,
+    SMALL,
+    BIG
+};
 
 template<typename>
 class function;
@@ -19,43 +28,73 @@ private:
         virtual ~base_holder() {};
 
         virtual T invoke(Args &&...) = 0;
+
         virtual std::unique_ptr<base_holder> copy() const = 0;
     };
 
     template<typename S>
     class holder : public base_holder {
     public:
-        holder(const S& t) : value(t) {}
-        holder(S&& t) : value(std::move(t)) {}
+        holder(const S &t) : value(t) {}
+
+        holder(S &&t) : value(std::move(t)) {}
+
         ~holder() = default;
-        T invoke(Args&&...args) {
+
+        T invoke(Args &&...args) {
             return value(std::forward<Args>(args)...);
         }
+
         std::unique_ptr<base_holder> copy() const {
             return std::make_unique<holder<S>>(value);
         }
+
     private:
         S value;
     };
-    std::unique_ptr<base_holder> func;
+
+    union {
+        std::unique_ptr<base_holder> func;
+        char buf[FUNCTION_BUFFER_SIZE];
+    };
+    TYPE type;
 public:
-    function() : func(nullptr){}
-    explicit function(std::nullptr_t) : func(nullptr) {}
-    function(const function & other) {
-        func = (std::move(other.func->copy()));
+    function() : func(nullptr), type(EMPTY) {}
+
+    explicit function(std::nullptr_t) : func(nullptr), type(EMPTY) {}
+
+    function(const function &other) : func(nullptr), type(other.type) {
+        switch (type) {
+            case EMPTY:
+                func = nullptr;
+                break;
+            case SMALL:
+                memcpy(this->buf, other.buf, FUNCTION_BUFFER_SIZE);
+                break;
+            case BIG:
+                func = (std::move(other.func->copy()));
+        }
     }
 
     function(function &&other) noexcept {
         this->swap(other);
     }
 
-    template <typename F>
-    function (F f) {
-        func = (std::make_unique<holder<F>>(std::move(f)));
+    template<typename F>
+    function(F f) {
+        if (sizeof(holder<F>(f)) <= FUNCTION_BUFFER_SIZE) {
+            type = SMALL;
+            new(this->buf) holder<F>(std::move(f));
+        } else {
+            type = BIG;
+            new(this->buf) std::unique_ptr<holder<F>>(std::make_unique<holder<F>>(std::move(f)));
+        }
     }
 
-    void swap(function& other) {
-        std::swap(this->func, other.func);
+    void swap(function &other) noexcept {
+        //std::swap(this->func, other.func);
+        std::swap(this->buf, other.buf);
+        std::swap(type, other.type);
     }
 
     explicit operator bool() const noexcept {
@@ -63,20 +102,33 @@ public:
     }
 
     T operator()(Args... args) const {
-        return func->invoke(std::forward<Args>(args)...);
+        switch (type) {
+            case EMPTY:
+                throw std::bad_function_call();
+            case SMALL:
+                return ((base_holder *) (buf))->invoke(std::forward<Args>(args)...);
+            case BIG:
+                return func->invoke(std::forward<Args>(args)...);
+        }
     }
 
     ~function() {
-        func.reset();
+        switch (type) {
+            case SMALL:
+                ((base_holder*)this->buf)->~base_holder();
+                break;
+            case BIG:
+                this->func.reset();
+        }
     }
 
-    function& operator=(const function& other) {
+    function &operator=(const function &other) {
         auto temp(other);
         this->swap(temp);
         return *this;
     }
 
-    function operator=(function&& other) noexcept {
+    function &operator=(function &&other) noexcept {
         this->swap(other);
         return *this;
     }
